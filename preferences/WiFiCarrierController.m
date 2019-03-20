0e3c44e0118db6c6c05002422ab1d26bbc24fc17
NoisyFlake <joelpoick@me.com>
2019-03-20 18:55:31 +0100

    Fix preference banner for landscape mode

--- preferences/WiFiCarrierController.m
+++ preferences/WiFiCarrierController.m
@@ -52,7 +52,7 @@
 		CGFloat width = 320;
 		CGFloat height = 70;
 
-		CGRect backgroundFrame = CGRectMake(0, -35, width, height);
+		CGRect backgroundFrame = CGRectMake(-50, -35, width+50, height);
 		background = [[UILabel alloc] initWithFrame:backgroundFrame];
 		[background layoutIfNeeded];
 		background.backgroundColor = [UIColor colorWithRed:0.30 green:0.85 blue:0.39 alpha:1.0];
